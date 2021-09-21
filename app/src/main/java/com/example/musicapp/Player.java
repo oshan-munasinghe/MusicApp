5d5639d5f72e9c19dd4edf102eb6f8db7bff0c9b
unknown <oshanmunasingha21@gmail.com>
2021-09-21 17:01:15 +0530

    add file images etc

---
+++ app/src/main/java/com/example/musicapp/Player.java
@@ -0,0 +1,13 @@
+package com.example.musicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Player extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
